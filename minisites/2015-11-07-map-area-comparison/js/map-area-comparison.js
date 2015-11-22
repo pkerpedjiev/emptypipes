7bac9538d2d34fa01944ec275fee46f2121b41d6
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2015-11-22 12:26:29 +0100

    Changes to map area comparison

--- minisites/2015-11-07-map-area-comparison/js/map-area-comparison.js
+++ minisites/2015-11-07-map-area-comparison/js/map-area-comparison.js
@@ -1,6 +1,7 @@
 function mapComparison() {
     var width = 400;
     var height = 400;
+    var scaleValue = null;
 
     function geoBounds(feature) {
         /* Get the bounding box of a GeoJSON feature
@@ -8,6 +9,7 @@ function mapComparison() {
          *
          * For some reason d3's geo.bounds didn't seem to work
          * */
+
         var minLat = 90;
         var maxLat = -90;
         var minLon = 180;
@@ -33,10 +35,14 @@ function mapComparison() {
         selection.each(function(data) {
             // add grid coordinates to the features
             var features = topojson.feature(data, data.objects.boundaries).features;
-            var numCols = features.length;
+            var numCols = features.length / 5;
+            var numRows = Math.ceil(features.length / numCols);
+
             var padding = [0,0];
             var nodeWidth = Math.min(height, Math.floor((width - (numCols - 1) * padding[0]) / numCols));
             var nodeHeight = nodeWidth;
+            var totalHeight = numRows * nodeHeight;
+            console.log('totalHeight:', totalHeight);
 
             var grid = d3.layout.grid().size([width,height])
             .cols(numCols)
@@ -56,20 +62,24 @@ function mapComparison() {
             var path = d3.geo.path()
                 .projection(projection);
 
-            var minS = 1000000000000;
 
-            for (var i = 0; i < features.length; i++) {
-                var feature = features[i];
-                var gb = geoBounds(feature);
-                var b = [projection(gb[0]), projection(gb[1])];
-                // scaling the projection taken from
-                // http://stackoverflow.com/a/17067379/899470
-                var s = 0.95 / Math.max(
-                        (b[1][0] - b[0][0]) / nodeWidth, 
-                        (b[0][1] - b[1][1]) / nodeHeight
-                    );
-                if (s < minS)
-                    minS = s;
+            if (scaleValue === null) {
+                scaleValue = 1000000000000;
+
+                for (var i = 0; i < features.length; i++) {
+                    var feature = features[i];
+                    var gb = geoBounds(feature);
+                    var b = [projection(gb[0]), projection(gb[1])];
+                    // scaling the projection taken from
+                    // http://stackoverflow.com/a/17067379/899470
+                    var s = 0.95 / Math.max(
+                            (b[1][0] - b[0][0]) / nodeWidth, 
+                            (b[0][1] - b[1][1]) / nodeHeight
+                        );
+                    if (s < scaleValue) {
+                        scaleValue = s;
+                    }
+                }
             }
             
             function createFeaturePath(feature) {
@@ -80,20 +90,30 @@ function mapComparison() {
 
                 projection.center([(b[1][0]+b[0][0])/2, (b[1][1]+b[0][1])/2]);
                 projection.translate([nodeWidth/2, nodeHeight/2]);
-                projection.scale(minS); 
+                projection.scale(scaleValue); 
+
+                console.log('feature', feature);
 
                 return path(feature.geometry);
             }
 
-            d3.select(this).selectAll(".subunit")
+            var gSkiArea = d3.select(this).selectAll(".subunit")
             .data(rectFeatures)
             .enter()
             .append('g')
             .attr('transform', function(d) { 
-                return 'translate(' + d.x + ',' + d.y + ')'; })
-            .append("path")
+                return 'translate(' + d.x + ',' + d.y + ')'; });
+
+            gSkiArea.append("path")
             .attr("class", function(d) { return "u" + d.properties.uid; })
             .attr("d", createFeaturePath);
+
+            gSkiArea.append('text')
+            .attr('transform', function(d) {
+                return 'translate(' + (nodeWidth / 2) + ',' + (nodeHeight + 5) + ')';
+            })
+            .classed('ski-area-name', true)
+            .text(function(d) { return d.properties.name; });
         });
     };
 
@@ -109,26 +129,42 @@ function mapComparison() {
         return chart;
     };
 
+    chart.scale = function(_) {
+        if (!arguments.length) return scaleValue;
+        scaleValue = _;
+        return chart;
+    };
+
     return chart;
 }
 
 function compareMaps(geoJson) {
+    /*
+    Create a grid comparing some topojson maps.
+
+    @param geoJson: The topojson file (misleading parameter name)
+    */
     var width = 550;
-    var height = 300;
+    var height = 550;
 
     var svg = d3.select('#map-area').append('svg')
         .attr('width', width)
         .attr('height', height);
 
+    var margin = {'top': 10, 'left': 10, 'right': 10, 'bottom': 10};
+
     var compareChart = mapComparison()
-    .width(width)
-    .height(height);
+    .width(width - margin.left - margin.right)
+    .height(height - margin.top - margin.bottom);
+
+    var prevArgs = arguments;
 
     d3.json(geoJson, function(data) {
         svg.selectAll('g')
         .data([data])
         .enter()
         .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
         .call(compareChart);
     });
 }
